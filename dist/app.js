97a0a44569a63a88a128de8fa99e2dac9c2731aa
arif <arifk62901@gmail.com>
2025-04-18 22:38:16 +0600

    middleware

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,12 +18,48 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.text());
 // :productId/:subId
-app.get("/", (req, res) => {
-    // console.log(req.params)
-    const { email, name } = req.query;
-    console.log(email);
-    res.send("We have no fronted so we can use postman for crud operation");
+// middleware
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/course", courseRouter);
+userRouter.get("/create-users", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User created Successfully",
+        User: user,
+    });
 });
+courseRouter.post("create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "Course created Successfully",
+        User: course,
+    });
+});
+const loggerMiddleware = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get("/", loggerMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // console.log(req.params)
+    try {
+        const { email, name } = req.query;
+        console.log(email);
+        res.send("We have no fronted so we can use postman for crud operation");
+    }
+    catch (error) {
+        console.log(error);
+        res.status(400).json({
+            success: false,
+            message: "Failed to get data",
+        });
+    }
+}));
 app.post("/", (req, res) => {
     console.log(" Postman to send data --> ", req.body);
     res.send("Get data");
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,61 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 
 // parser use to send data to parse
 app.use(express.json());
 app.use(express.text());
 // :productId/:subId
-app.get("/", (req: Request, res: Response) => {
+
+// middleware
+
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/course", courseRouter);
+
+userRouter.get("/create-users", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User created Successfully",
+    User: user,
+  });
+});
+
+courseRouter.post("create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+
+  res.json({
+    success: true,
+    message: "Course created Successfully",
+    User: course,
+  });
+});
+
+const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
+
+app.get("/", loggerMiddleware,async (req: Request, res: Response) => {
   // console.log(req.params)
-  const {email,name} = req.query
-  console.log(email)
-  res.send("We have no fronted so we can use postman for crud operation");
+  try {
+    const { email, name } = req.query;
+    console.log(email);
+    res.send("We have no fronted so we can use postman for crud operation");
+  } catch (error) {
+
+
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: "Failed to get data",
+    });
+  }
 });
 
 app.post("/", (req: Request, res: Response) => {
